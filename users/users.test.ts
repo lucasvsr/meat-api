053319fa4f8e8851cba9eb805f54746ee3b206bb
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2020-04-26 08:29:52 -0300

    Aula 50

--- users/users.test.ts
+++ users/users.test.ts
@@ -54,9 +54,52 @@ test('post /users', () => {
                           expect(response.body.cpf).toBe('227.686.630-69')
                           expect(response.body.password).toBeUndefined()
 
-                       }).catch(fail)
+                       })
+                       .catch(fail)
  
  })
+
+test('get /users/[ID QUE NAO EXISTE]', () => {
+
+    return request(url).get('/users/aaaaa')
+                      .then(response => {
+
+                         expect(response.status).toBe(404)
+                         
+                      }).catch(fail)
+
+
+})
+
+test('patch /users/:id', () => {
+
+    return request(url).post('/users')
+                       .send({
+
+                            name:"Melisenda Ribeiro",
+                            email:"mribeiro@boston.com",
+                            password:"tY9MSY",
+                            gender:"Female",
+                            cpf:"227.686.630-69"
+
+                       })
+                       .then(response => request(url).patch(`/users/${response.body._id}`)
+                                                     .send({
+
+                                                        name:"Melisenda Magnay R"
+
+                                                     }))
+                       .then(response => {
+
+                            expect(response.status).toBe(200)
+                            expect(response.body._id).toBeDefined()
+                            expect(response.body.name).toBe('Melisenda Magnay R')
+                            expect(response.body.email).toBe('mribeiro@boston.com')
+
+                       })
+                       .catch(fail)
+
+})
 
  afterAll(() => {
 
