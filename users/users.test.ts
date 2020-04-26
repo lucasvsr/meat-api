8b2e99da37338aac5a2acf7f19feaf93b3eeab37
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2020-04-26 08:03:55 -0300

    Aula 50

---
+++ users/users.test.ts
@@ -0,0 +1,65 @@
+import { User } from './users.model';
+import { usersRouter } from './users.router';
+import { environment } from './../common/environment';
+import { Server } from './../server/server';
+import 'jest'
+import * as request from 'supertest'
+
+let url: string
+let server: Server
+
+beforeAll(() => {
+
+    environment.db.url = process.env.DB_URL || 'mongodb://localhost/meat-api-test-db'
+    environment.server.port = process.env.SERVER_PORT || 3001
+    url = `http://localhost:${environment.server.port}`
+    server = new Server()
+
+    return server.bootstrap([usersRouter]) //INICIAR O SERVIDOR COM AS ROTAS DE USER
+                 .then(() => User.remove({}).exec()) //APAGA TODA A BASE
+                 .catch(console.error) //IMPRIME QUALQUER ERRO QUE OCORRA
+
+})
+
+test('get /users', () => {
+
+   return request(url).get('/users')
+                      .then(response => {
+
+                         expect(response.status).toBe(200)
+                         expect(response.body.items).toBeInstanceOf(Array)
+                         
+                      }).catch(fail)
+
+})
+
+test('post /users', () => {
+
+    return request(url).post('/users')
+                       .send({
+
+                            name:"Melisenda Magnay",
+                            email:"mmagnay0@boston.com",
+                            password:"tY9MSY",
+                            gender:"Female",
+                            cpf:"227.686.630-69"
+
+                       })
+                       .then(response => {
+ 
+                          expect(response.status).toBe(200)
+                          expect(response.body._id).toBeDefined()
+                          expect(response.body.name).toBe('Melisenda Magnay')
+                          expect(response.body.email).toBe('mmagnay0@boston.com')
+                          expect(response.body.cpf).toBe('227.686.630-69')
+                          expect(response.body.password).toBeUndefined()
+
+                       }).catch(fail)
+ 
+ })
+
+ afterAll(() => {
+
+    return server.shutdown() //DESLIGA O SERVIDOR
+
+ })
